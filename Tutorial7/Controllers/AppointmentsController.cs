8edb9f3dc400b53fa5590d184d4651c82d469b84
Pawel-coder <s33980@pjwstk.edu.pl>
2026-04-22 22:14:12 +0200

    Removing all WeatherForecast example code, adding AppointmentsController placeholder

---
+++ Tutorial7/Controllers/AppointmentsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial7.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AppointmentsController : ControllerBase
+{
+    
+}
